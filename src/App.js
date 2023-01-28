8593a2a507108efa9ff755dec8da79e25925d19d
Liam King <liamking@live.co.uk>
2023-01-28 12:49:39 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Liam King</h1>
+      <h2>Portfolio Under Construction</h2>
+
+    </div>
+  );
+}
+
+export default App;
